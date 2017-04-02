47caeb997511f7f2a0a86fc832fd79173218d11b
AntiNomyKim <myp0816@naver.com>
2017-04-03 07:17:38 +0900

    솔루션 추가.

---
+++ ANK_201704_Challenge/ANK_201704_ChallengePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ANK_201704_Challenge
+{
+	public partial class ANK_201704_ChallengePage : ContentPage
+	{
+		public ANK_201704_ChallengePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ ANK_201704_Challenge/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace ANK_201704_Challenge
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new ANK_201704_ChallengePage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
